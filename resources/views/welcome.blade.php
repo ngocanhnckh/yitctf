ab4fed46b4463549f650a0efa88460432db08491
MrNA <ngocanhnckh@gmail.com>
2018-07-29 17:57:57 +0700

    fix design error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,7 +88,7 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #fff;
                 padding: 0 25px;
                 font-size: 12px;
                 font-weight: 600;
